2c42bd0c0102d1896970f81e1f7adfa399089825
Arthur Dos Reis <arthurreis120@gmail.com>
2023-03-21 21:15:11 -0300

    Atividade

--- Lista sequencial/2.c
+++ Lista sequencial/2.c
@@ -6,6 +6,7 @@ int main(){
 
     printf("Caderno de poesias � um belo lugar.\nTantas coisas lindas que eu gostaria de falar.\nEu falo em forma de versos para todos poderem escutar.\nAgora voc� j� sabe por que os poetas passam os dias escrevendo em seus cadernos de poesias.");
 
+
     return 0;
 
 }
