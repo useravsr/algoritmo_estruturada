110b94fbeea8a7397db8e77b79677ab0605edf3e
Arthur Dos Reis <arthurreis120@gmail.com>
2023-03-28 21:34:32 -0300

    Atividade

---
+++ string.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+
+    char nome[100];
+    char sobrenome[100];
+
+    scanf("%s %s", nome, sobrenome);
+
+    if(strlen(nome) > strlen(sobrenome)) {
+        
+        printf("Frase 1 maior que frase 2");
+        
+    }else printf("Frase 2 maior que frase 1");
+
+    if(strlen(nome) == strlen(sobrenome)) printf("As frases posuem o mesmo tamanho");
+        
+    return 0;
+}
