ddb2344c8c2229fafea4403534352a3e53422418
Arthur Dos Reis <arthurreis120@gmail.com>
2023-04-04 19:56:40 -0300

    Atividade

---
+++ Lista Repeticao/2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+    int n1;
+
+    printf("Insira um numero:");
+    scanf("%d", &n1);
+
+    for(int i = n1; i!=0 ; i--){
+
+        printf("%d\n",i);
+
+    }
+
+    return 0;
+}
