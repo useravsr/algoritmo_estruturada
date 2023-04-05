43d48fbf19d9f6428e6f271f06d720a64942ab58
Arthur Dos Reis <arthurreis120@gmail.com>
2023-04-04 21:11:04 -0300

    Atividade

---
+++ Lista Repeticao/4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+
+    int n1=0;
+
+    for(int i = 0; i < 5 ; i++){
+
+        n1+=3;
+
+        if(n1 != 0){
+
+            printf("%d\n", n1);
+
+        }
+
+    }
+
+    return 0; 
+}
